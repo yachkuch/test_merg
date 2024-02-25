9ff6a39fbfe30c84ccfba2504f6f28d8bc534bbe
Nikita Yakush <yachkuch@gmail.com>
2024-02-25 13:03:55 +0300

    fix 5

--- main.cpp
+++ main.cpp
@@ -5,4 +5,8 @@ int main(int, char**){
     std::cout << "Hello, from testing_local_git_rep!\n";
     std::cout << "Fitch 1 from master!\n";
     std::cout << "Fitch 4 from master!\n";
+    std::cout << "A lot of fun!\n";
+    std::cout << "A lot of fun!\n";
+    std::cout << "A lot of fun!\n";
+    std::cout << "A lot of fun!\n";
 }
