d658a8064e3accb3fc2241f6296b1a32aed9fa25
Nikita Yakush <yachkuch@gmail.com>
2024-02-25 13:00:40 +0300

    fitch 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 int main(int, char**){
-    std::cout << "Hello, from Testing_local_git_rep!\n";
+    std::cout << "Hello, from testing_local_git_rep!\n";
     std::cout << "Fitch 1 from master!\n";
+    std::cout << "Fitch 4 from master!\n";
 }
